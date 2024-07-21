6b41efd752c1c67703ecb33fae3e7af77d478e9a
DIM <4ebotarov_dim@inbox.ru>
2024-07-21 21:46:20 +0300

    A+B char

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ // testing 1
+int main(int argc, char** argv) {
+ char simvol1 = 'A';
+ char simvol2 = 'B';
+ char simvol = '0';
+ 
+ simvol=simvol1+simvol2;
+ 
+ cout<<"Simvol= "<<simvol<<endl;
+ 	
+system("pause>0");	
+	return 0;
+}
