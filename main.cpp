ba8778a80540d00ea34ed495416b629a447f22e2
Dmitriy <127180093+chebotarevdmitr@users.noreply.github.com>
2024-07-21 22:29:23 +0300

    saze of bytes

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 using namespace std;
- // testing 1
-int main(int argc, char** argv) {
- char simvol1 = 'A';
- char simvol2 = 'B';
- char simvol = '0';
- 
- simvol=simvol1+simvol2;
- 
- cout<<"Simvol= "<<simvol<<endl;
- 	
-system("pause>0");	
+//razmer 
+int main(){
+	cout<<"Size char= "<<sizeof(char)<<" byte"<<endl;
+	cout<<"Size int= " <<sizeof(int )<<" byte"<<endl;
+	cout<<"Size float= "<<sizeof(float)<<" byte"<<endl;
+	cout<<"Size double= "<<sizeof(double)<<" byte"<<endl;
+	cout<<"Size long int= "<<sizeof(long int)<<" byte"<<endl;
+	cout<<"Size long long int= "<<sizeof(long long int )<<" byte"<<endl;
+	
+	system("pause>0");
 	return 0;
 }
