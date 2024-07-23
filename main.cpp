20050acc9dcdb4f109c7fb97d7accc4926a5d583
DIM <4ebotarov_dim@inbox.ru>
2024-07-23 21:41:30 +0300

    пост и преф

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 using namespace std;
-//privedenie tipov
+//использование инкремета идекремента 
 int main(){
-	char c = '0';
-	cout<< "Enter Simvol :";
-	cin >> c;
-	cout<<"ASCII ="<<int(c)<<endl;
+	int a =0,b=0;
+	int i=4,j=4;
+	cout <<"Znachenie peremennyh v samom nachale \n";
+	cout <<"i= "<<i<<endl;
+	cout <<"j= "<<j<<endl;
 	
+	cout<< "znachenie posle n =i++ \n";
+	a=i++;
+	cout<<"a= "<<a<<"\n";
+	cout<<"i= "<<i<<"\n";
+	
+	cout<< "znachenie posle m = ++j \n";
+	b=++j;
+	cout<<"b= "<<b<<"\n";
+	cout<<"j= "<<j<<"\n";
 	system("pause>0");
 	return 0;
 }
